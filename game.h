3c1ae487f35eff392b9916353a70df972f06bd80
jackjj1125 <77815395+jackjj1125@users.noreply.github.com>
2021-11-08 15:11:08 -0700

    Update game.h
    Adding to game.h file, fixing getters and setters

--- game.h
+++ game.h
@@ -1,25 +1,51 @@
 #ifndef GAME_H
 #define GAME_H
+#include <QMainWindow>
+#include <QColor>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsItem>
+#include <mainwindow.h>
 
 
-class game
+class game: public QObject, public QGraphicsItem
 {
-public:
-    game(int x, int y, int width, int height);
-    int get_x();
-    int get_y();
-    int get_width();
-    int get_height();
-    int get_status();
-    int get_neighborStatus();
-    void set_status();
-    void set_NeighborStatus();
-    void set_Color();
 
-signals:
+    Q_OBJECT
+
+public:
+    game(int x, int y, int width, int height); //constructor
+
+    QRectF boundingRect() const override;
+    QPainterPath shape() const override;
+
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *item, QWidget *widget) override;
+
+
+    int get_x() {return x_;}; //getter for x coord
+    int get_y(){return y_;}; //getter for y cord
+
+    int get_width() {return width_;}; //getter for cell width
+    int get_height() {return height_;};
+
+    int get_status() {return is_alive;}; //getter for status of cell(alive or dead)
+    int get_neighborStatus(); //getter for status of cell's neighbor
+    int get_nextStatus()  {return next_turn;};
+
+    void set_status(bool is_alive_) {is_alive = is_alive_;}; //setter for status of cell
+    void set_NeighborStatus(); //setter for status of neighbor
+    void set_nextStatus(bool next_turn_) {next_turn = next_turn_;}; //sets status for next turn
+
+    QColor get_color() {return color_;}; //getter for color of cell
+    void set_Color(int r, int g, int b); //setter for color of cell(handles color logic)
+
     void increase();
     void decrease();
     void neighbors(int x, int y);
+
+
+protected:
+    void mousePressEvent(QGraphicsSceneMouseEvent * event) override;
 
 private:
     int x_;
@@ -27,6 +53,7 @@ private:
     int width_;
     int height_;
     bool is_alive;
+    bool next_turn;
     QColor color_;
 
 
