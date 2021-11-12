c7880498152c5a2f2d230a98e116749af1f23523
jackjj1125 <77815395+jackjj1125@users.noreply.github.com>
2021-11-11 18:07:49 -0700

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,8 @@
 #include <QGraphicsScene>
 #include <QGraphicsView>
 #include <QTimer>
+#include <QtWidgets>
+#include <vector>
 
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
@@ -13,6 +15,10 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // set up timer and set start flag to false
+    initTimer();
+    start_ = false;
 
     turnCounter_ = 0;
     QColor color(255,0,0);
@@ -34,17 +40,18 @@ MainWindow::MainWindow(QWidget *parent)
         {
             game * item = new game(j,i,cell_width_/20, cell_height_/10);
             cells[i][j] = item;
+            cells[i][j]->set_nextStatus(-1);
             MakeBoard_->addItem(item);
-           // connect(item, &game::increase, this, &MainWindow::receive_inc);
-           // connect(item, &game::decrease, this, &MainWindow::receive_dec);
+            connect(item, &game::reviveCell, this, &MainWindow::clickCellSlot);
+            connect(item, &game::killCell, this, &MainWindow::clickCellSlot);
 
-            if(item->get_color() == QColor(240,20,120))
+            if(item->get_status())
             {
-                PopulationCounter(1);
+                increasePopulation();
             }
         }
     }
-    ui->label->setText(QString("Turn#: ")+QString::number(turnCounter_));
+    ui->label->setText(QString("Turn: ")+QString::number(turnCounter_));
     ui->label_2->setText(QString("Population: ")+QString::number(population_) + QString(" (") + QString::number((population_ * 100)/200) +QString("%" ));
 
 
@@ -53,6 +60,12 @@ MainWindow::MainWindow(QWidget *parent)
 //    graph_view->setScene(MakePopGraph_);
 //    graph_view->setSceneRect(0,0,graph_view->frameSize().width(), graph_view->frameSize().height());
 
+    connect(ui->playButton, &QAbstractButton::pressed, this, &MainWindow::on_playButton_click);
+    connect(ui->pauseButton, &QAbstractButton::pressed, this, &MainWindow::on_pauseButton_click);
+    connect(ui->stepButton, &QAbstractButton::pressed, this, &MainWindow::on_stepButton_click);
+    connect(ui->speedSlider, &QAbstractSlider::sliderMoved, this, &MainWindow::speedSliderMoved);
+    connect(ui->restartButton, &QAbstractButton::pressed, this, &MainWindow::on_restartButton_click);
+
 }
 
 MainWindow::~MainWindow()
@@ -60,9 +73,27 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::PopulationCounter(int pop)
-{
-    population_ += pop;
+void MainWindow::initTimer(){
+    timer = new QTimer(this);
+    timer->setInterval(1000);
+    connect(timer, SIGNAL(timeout()), this, SLOT(on_stepButton_click()));
+}
+
+void MainWindow::changeInterval(int t){
+    if(t < 2){
+        return;
+    }
+
+    timer->setInterval(1000 / t);
+}
+
+void MainWindow::increasePopulation(){
+    population_++;
+    ui->label_2->setText(QString("Population: ")+QString::number(population_)+QString(" ") +QString::number((population_ * 100)/200)+QString("%"));
+
+}
+void MainWindow::decreasePopulation(){
+    population_--;
     ui->label_2->setText(QString("Population: ")+QString::number(population_)+QString(" ") +QString::number((population_ * 100)/200)+QString("%"));
 
 }
@@ -70,15 +101,212 @@ void MainWindow::PopulationCounter(int pop)
 void MainWindow::turnCount() //handles number of turns
 {
     turnCounter_++; //increment turn counter
-   // ui->label->setText(QString("Turn#: ")+QString::number(turnCounter_)); //print number turn
+    ui->label->setText(QString("Turn#: ")+QString::number(turnCounter_)); //print number turn
+}
+
+std::vector<game*> MainWindow::getNeighbors(int i, int j){
+    std::vector<game*> neighbors;
+
+    if(i == 0 && j == 0){  // top left corner
+        neighbors.push_back(cells[9][19]);
+        neighbors.push_back(cells[9][j]);
+        neighbors.push_back(cells[9][j+1]);
+
+        neighbors.push_back(cells[i][19]);
+        neighbors.push_back(cells[i][j+1]);
+
+        neighbors.push_back(cells[i+1][19]);
+        neighbors.push_back(cells[i+1][j]);
+        neighbors.push_back(cells[i+1][j+1]);
+
+        return neighbors;
+    }
+    else if(i == 9 && j == 19){ // bottom right corner
+        neighbors.push_back(cells[i-1][j-1]);
+        neighbors.push_back(cells[i-1][j]);
+        neighbors.push_back(cells[i-1][0]);
+
+        neighbors.push_back(cells[i][j-1]);
+        neighbors.push_back(cells[i][0]);
+
+        neighbors.push_back(cells[0][j-1]);
+        neighbors.push_back(cells[0][j]);
+        neighbors.push_back(cells[0][0]);
+
+        return neighbors;
+    }
+    else if(i == 0 && j == 19){ // top right corner
+        neighbors.push_back(cells[9][j-1]);
+        neighbors.push_back(cells[9][j]);
+        neighbors.push_back(cells[9][19]);
+
+        neighbors.push_back(cells[i][j-1]);
+        neighbors.push_back(cells[i][0]);
+
+        neighbors.push_back(cells[i+1][j-1]);
+        neighbors.push_back(cells[i+1][j]);
+        neighbors.push_back(cells[i+1][0]);
+
+        return neighbors;
+    }
+    else if(i == 9 && j == 0){ // bottom left corner
+        neighbors.push_back(cells[i-1][19]);
+        neighbors.push_back(cells[i-1][j]);
+        neighbors.push_back(cells[i-1][j+1]);
+
+        neighbors.push_back(cells[i][19]);
+        neighbors.push_back(cells[i][j+1]);
+
+        neighbors.push_back(cells[0][19]);
+        neighbors.push_back(cells[0][j]);
+        neighbors.push_back(cells[0][j+1]);
+
+        return neighbors;
+    }
+    else if(i == 0 && j != 0 && j != 19){  // top row, no corners
+        neighbors.push_back(cells[9][j-1]);
+        neighbors.push_back(cells[9][j]);
+        neighbors.push_back(cells[9][j+1]);
+
+        neighbors.push_back(cells[i][j-1]);
+        neighbors.push_back(cells[i][j+1]);
+
+        neighbors.push_back(cells[i+1][j-1]);
+        neighbors.push_back(cells[i+1][j]);
+        neighbors.push_back(cells[i+1][j+1]);
+
+        return neighbors;
+    }
+    else if(i != 0 && i != 9 && j == 0){ // far right column, no corners
+        neighbors.push_back(cells[i-1][19]);
+        neighbors.push_back(cells[i-1][j]);
+        neighbors.push_back(cells[i-1][j+1]);
+
+        neighbors.push_back(cells[i][19]);
+        neighbors.push_back(cells[i][j+1]);
+
+        neighbors.push_back(cells[i+1][19]);
+        neighbors.push_back(cells[i+1][j]);
+        neighbors.push_back(cells[i+1][j+1]);
+
+        return neighbors;
+    }
+    else if(i == 9 && j != 19 && j != 0){ // bottom row, no corners
+        neighbors.push_back(cells[i-1][j-1]);
+        neighbors.push_back(cells[i-1][j]);
+        neighbors.push_back(cells[i-1][j+1]);
+
+        neighbors.push_back(cells[i][j-1]);
+        neighbors.push_back(cells[i][j+1]);
+
+        neighbors.push_back(cells[0][j-1]);
+        neighbors.push_back(cells[0][j]);
+        neighbors.push_back(cells[0][j+1]);
+
+        return neighbors;
+    }
+    else if(i != 0 && i != 9 && j == 19){ // far left column, no corners
+        neighbors.push_back(cells[i-1][j-1]);
+        neighbors.push_back(cells[i-1][j]);
+        neighbors.push_back(cells[i-1][0]);
+
+        neighbors.push_back(cells[i][j-1]);
+        neighbors.push_back(cells[i][0]);
+
+        neighbors.push_back(cells[i+1][j-1]);
+        neighbors.push_back(cells[i+1][j]);
+        neighbors.push_back(cells[i+1][0]);
+
+        return neighbors;
+    }
+    else{ // no edges or corners
+        neighbors.push_back(cells[i-1][j-1]);
+        neighbors.push_back(cells[i-1][j]);
+        neighbors.push_back(cells[i-1][j+1]);
+
+        neighbors.push_back(cells[i][j-1]);
+        neighbors.push_back(cells[i][j+1]);
+
+        neighbors.push_back(cells[i+1][j-1]);
+        neighbors.push_back(cells[i+1][j]);
+        neighbors.push_back(cells[i+1][j+1]);
+    }
+
+    return neighbors;
+}
+
+int MainWindow::checkNeighbors(std::vector<game*> neighbors){
+    int aliveCount = 0;
+
+    for(int i = 0; i < neighbors.size(); i++){
+        if(neighbors[i]->get_status()){
+            aliveCount++;
+        }
+    }
+
+    if(aliveCount < 2){
+        return 1;
+    }
+    if(aliveCount == 2){
+        return 2;
+    }
+    if(aliveCount > 3){
+        return 3;
+    }
+    if(aliveCount == 3){
+        return 4;
+    }
+
 }
 
 void MainWindow::checkAlive() //determine if cells should be dead or alive
 {
+    //qDebug() << "checking if cells are dead or alive";
 
+
+    int status = 0;
+
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 20; j++){
+            status = checkNeighbors(getNeighbors(i, j));
+
+            if(cells[i][j]->get_status()){
+                if(status == 1 || status == 3){
+                    // kill cell
+                    cells[i][j]->set_nextStatus(0);
+                }
+                // if status == 2, do nothing
+            }
+            else{
+                if(status == 4){
+                    // make cell alive
+                    cells[i][j]->set_nextStatus(1);
+                }
+            }
+        }
+    }
+    qDebug() << "next status set";
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 20; j++){
+            if(cells[i][j]->get_nextStatus() == 1){
+                cells[i][j]->revive();
+            }
+            else if(cells[i][j]->get_nextStatus() == 0){
+                cells[i][j]->kill();
+            }
+            cells[i][j]->set_nextStatus(-1);
+        }
+    }
 }
 
-
+void MainWindow::clickCellSlot(game * cell){
+    if(cell->get_status()){
+        increasePopulation();
+    }
+    else{
+        decreasePopulation();
+    }
+}
 
 //bool MainWindow::is_alive() //checks if cells are alive
 //{
@@ -87,24 +315,52 @@ void MainWindow::checkAlive() //determine if cells should be dead or alive
 
 
 
-//void MainWindow::on_restartButton_click() //button for restarting game
-//{
+void MainWindow::on_restartButton_click() //button for restarting game
+{
+    qDebug() << "Restart Game Button Clicked";
+    if(start_)
+    {
+        initTimer();
+        start_ = false;
+        if(!start_)
+        {
+            timer->start();
+            start_ = true;
+        }
+    }
+}
 
-//}
+//button for starting game
+void MainWindow::on_playButton_click(){
+    qDebug() << "Play button clicked!";
+
+    if(!start_){
+        timer->start();
+        start_ = true;
+    }
+}
 
 
-//void MainWindow::on_playButton_click() //button for starting game
-//{
+void MainWindow::on_pauseButton_click(){
+    qDebug() << "Pause button clicked!";
+    if(start_){
+        timer->stop();
+        start_ = false;
+    }
+}
 
-//}
+void MainWindow::on_stepButton_click(){
+    //qDebug() << "Step button clicked!";
+    checkAlive();
+    turnCount();
+}
 
-//void MainWindow::on_pauseButton_click()
-//{
-//    timer->stop();
-//}
-
-//void MainWindow::on_stepButton_click()
-//{
-    //checkAlive();
-//}
-
+void MainWindow::speedSliderMoved(int position){
+    if(position < 2){
+        ui->speedLabel->setText(QString("Speed: 1 sec"));
+    }
+    else{
+        changeInterval(position);
+        ui->speedLabel->setText(QString("Speed: ")+QString::number(1000 / position)+QString(" ms"));
+    }
+}
