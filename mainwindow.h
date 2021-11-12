84dcaf0ccaa0e6cacefb0895130264eed1ba3e99
jackjj1125 <77815395+jackjj1125@users.noreply.github.com>
2021-11-11 18:08:13 -0700

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -21,22 +21,36 @@ public:
 
     void turnCount(); //handles turns
     void checkAlive(); //checks if cells should be alive or dead
-    int NeighborCount(); //checks neighbors
+    int checkNeighbors(std::vector<game*> neighbors); //checks neighbors
+    std::vector<game*> getNeighbors(int i, int j);
     bool is_alive(); //checks if a given cell is alive
-    void PopulationCounter(int pop);
+    void increasePopulation();
+    void decreasePopulation();
    // void receive_inc(); //
    // void receive_dec(); //
     void neighborSlot(int x, int y);
 
     int population_;
 
+    void initTimer();
+    void changeInterval(int t);
+
 
 private slots: //buttons
 
- //   void on_restartButton_click(); //slot for restart game button
-   // void on_playButton_click(); //slot for play game button
-    //void on_pauseButton_click(); //slot for pause game button
-    //void on_slider(int value); //slot for slider button
+//    void on_restartButton_click(); //slot for restart game button
+
+
+    void on_playButton_click(); //slot for play game button
+    void on_pauseButton_click(); //slot for pause game button
+    void on_stepButton_click();
+    void on_restartButton_click();
+
+    void clickCellSlot(game * cell);
+
+
+    void speedSliderMoved(int position);
+
 
 private:
     Ui::MainWindow *ui;
@@ -49,6 +63,9 @@ private:
     int cell_width_;
     int turnCounter_ = 0;
     QTimer *timer;
+
     game * cells[10][20];
+
+    bool start_;
 };
 #endif // MAINWINDOW_H
