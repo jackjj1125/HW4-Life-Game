852c7e34e68b575b4a34c0cbd9ad8ca9db2dce30
jackjj1125 <77815395+jackjj1125@users.noreply.github.com>
2021-11-11 18:06:54 -0700

    Update game.cpp
    adding completed game logic

--- game.cpp
+++ game.cpp
@@ -27,6 +27,29 @@ game::game(int x, int y, int width, int height) //constructor
 
 }
 
+void game::kill(){
+    if(!is_alive){
+        return;
+    }
+    is_alive = false;
+    QColor newColorD(255, 255, 255);
+    color_ = newColorD;
+
+    emit killCell(this);
+    update();
+}
+void game::revive(){
+    if(is_alive){
+        return;
+    }
+    is_alive = true;
+    QColor newColorA(120,20,140);
+    color_ = newColorA;
+
+    emit reviveCell(this);
+    update();
+}
+
 
 QRectF game::boundingRect() const
 {
@@ -57,100 +80,41 @@ void game::mousePressEvent(QGraphicsSceneMouseEvent *event)
 {
     if(event->button() == Qt::RightButton)
     {
-        if(this->get_color() == QColor(120, 20, 140))
+        if(!this->get_status())
         {
             qDebug() << "Dead Cell";
-           // this->set_Color(255, 255, 255);
-            emit decrease();
+
+        }
+        else{
+            qDebug() << "You killed this cell";
+            this->set_status(false);
+            QColor newColorD(255, 255, 255);
+            color_ = newColorD;
+
+
+            emit killCell(this);
         }
     }
     else if(event->button() == Qt::LeftButton)
     {
-        if(this->get_color() == QColor(255,255,255))
+        if(!this->get_status())
         {
-             qDebug() << "Cell is now alive";
-           //  this->set_Color(120,20,140);
-             emit increase();
+            qDebug() << "Cell has been revived";
+            QColor newColorA(120,20,140);
+            color_ = newColorA;
+            this->set_status(true);
+
+
+            emit reviveCell(this);
+        }
+        else{
+            qDebug() << "Cell is alive";
         }
     }
-                update();
-
-
-
+    update();
 }
 
 
-
-
-
-    if(rand() % 100 < 50)
-    {
-        color.setRgb(120,20,140);
-        set_status(true);
-    }
-    else
-    {
-        color.setRgb(255,255,255);
-        set_status(false);
-    }
-    color_ = color;
-    x_ = x * width;
-    y_ = y * height;
-    width_ = width;
-    height_ = height;
-
-}
-
-QRectF game::boundingRect() const
-{
-    return QRectF(x_, y_, width_, width_);
-
-}
-
-QPainterPath game::shape() const
-{
-    QPainterPath path;
-    path.addRect(x_, y_, width_, width_);
-    return path;
-
-}
-
-void game::paint(QPainter *painter, const QStyleOptionGraphicsItem *item, QWidget *widget)
-{
-    Q_UNUSED(widget)
-
-
-    painter->setBrush(QBrush(color_));
-    painter->drawRect(QRect(this->x_, this->y_, this->width_, this->width_));
-
-
-}
-
-void game::mousePressEvent(QGraphicsSceneMouseEvent *event)
-{
-    if(event->button() == Qt::RightButton)
-    {
-        if(this->get_color() == QColor(120, 20, 140))
-        {
-            qDebug() << "Dead Cell";
-           // this->set_Color(255, 255, 255);
-            emit decrease();
-        }
-    }
-    else if(event->button() == Qt::LeftButton)
-    {
-        if(this->get_color() == QColor(255,255,255))
-        {
-             qDebug() << "Cell is now alive";
-           //  this->set_Color(120,20,140);
-             emit increase();
-        }
-    }
-                update();
-
-
-
-}
 
 
 
@@ -163,5 +127,3 @@ void game::mousePressEvent(QGraphicsSceneMouseEvent *event)
     //Any dead cell with exactly three live niehgbors becomes a live cell
 
     //cells initalize with 50% chance of being alive or dead
-
-
